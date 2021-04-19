855484e4b2af500a2da42a661e2a2b882e56f94c
LiselotJansen <80678991+LiselotJansen@users.noreply.github.com>
2021-04-19 22:39:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Pizza:
+const maakPizza = function () { // Writing or 'defining' a function
+    console.log("maak deeg");
+    console.log("draai de pizza");
+    console.log("besmeer met tomatensaus");
+    console.log("beleg de pizza");
+    console.log("stop pizza in de oven");
+};
+maakPizza(); // Using or "calling" a function
+maakPizza();
+
+
+// Sushi:
+const maakSushi = function () {
+    console.log("kook rijst");
+    console.log("leg de rijst en ingredienten op het sushivel");
+    console.log("maak er een rol van");
+    console.log("snij de rol in stukjes")
+}
+
+maakSushi();
+maakSushi();
+maakSushi();
